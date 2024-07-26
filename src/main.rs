5af307a6c0a0d8511b98d08efbf9e5e3b3487853
Dillon-Roller <dillonroller.18@gmail.com>
2024-07-25 22:44:51 -0400

    scheduler

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,20 @@
 mod api;
 mod db;
 mod progress_bar;
+mod scheduler;
 
 use crate::api::Api;
 use crate::db::Database;
 use anyhow::Result;
 use env_logger::{Builder, Env};
 use tokio::signal;
-use tokio_cron_scheduler::{Job, JobScheduler};
+use scheduler::Scheduler;
 
 fn setup_env() -> Result<()> {
     // Logger
     Builder::from_env(Env::default().default_filter_or("info")).init();
     // Environment variables
     dotenvy::dotenv()?;
-    Ok(())
-}
-async fn setup_jobs(scheduler: &mut JobScheduler, api: Api, db: Database) -> Result<()> {
-    let job = Job::new_async("0 0 0 * * * *", move |_uuid, _l| {
-        let api = api.clone();
-        let db = db.clone();
-        Box::pin(async move {
-            update_skins(api, db)
-                .await
-                .map_err(|e| log::error!("{e}"))
-                .ok();
-        })
-    })?;
-
-    scheduler.add(job).await?;
-    Ok(())
-}
-
-async fn update_skins(api: Api, db: Database) -> Result<()> {
-    for skin in api.get_skins().await? {
-        db.store_skin(&skin).await?;
-    }
-
-    log::info!("Stored skins to database");
     Ok(())
 }
 
@@ -48,8 +25,8 @@ async fn main() -> Result<()> {
     let api = Api::new();
     let db = Database::new().await?;
 
-    let mut scheduler = JobScheduler::new().await?;
-    setup_jobs(&mut scheduler, api, db).await?;
+    let mut scheduler = Scheduler::new().await?;
+    scheduler.setup_jobs(api, db).await?;
     scheduler.start().await?;
 
     signal::ctrl_c().await?;
---
+++ src/scheduler.rs
@@ -0,0 +1,49 @@
+use crate::api::Api;
+use crate::db::Database;
+use anyhow::Result;
+use tokio_cron_scheduler::{Job, JobScheduler};
+
+pub(crate) struct Scheduler {
+    scheduler: JobScheduler,
+}
+
+impl Scheduler {
+    pub(crate) async fn new() -> Result<Self> {
+        let scheduler = JobScheduler::new().await?;
+        Ok(Self { scheduler })
+    }
+
+    pub(crate) async fn setup_jobs(&mut self, api: Api, db: Database) -> Result<()> {
+        let job = Job::new_async("0 0 0 * * * *", move |_uuid, _l| {
+            let api = api.clone();
+            let db = db.clone();
+            Box::pin(async move {
+                Self::update_skins(api, db)
+                    .await
+                    .map_err(|e| log::error!("{e}"))
+                    .ok();
+            })
+        })?;
+        
+        self.scheduler.add(job).await?;
+        Ok(())
+    }
+
+    pub(crate) async fn start(&mut self) -> Result<()> {
+        self.scheduler.start().await?;
+        Ok(())
+    }
+
+    pub(crate) async fn shutdown(&mut self) -> Result<()> {
+        self.scheduler.shutdown().await?;
+        Ok(())
+    }
+
+    async fn update_skins(api: Api, db: Database) -> Result<()> {
+        for skin in api.get_skins().await? {
+            db.store_skin(&skin).await?;
+        }
+        log::info!("Stored skins to database");
+        Ok(())
+    }
+}
