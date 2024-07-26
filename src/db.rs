78675d684eb2d05c63585a99faadddd26ebda55e
Dillon-Roller <dillonroller.18@gmail.com>
2024-07-25 20:33:23 -0400

    refactor

--- src/db.rs
+++ src/db.rs
@@ -7,27 +7,27 @@ use sqlx::{
 use std::env;
 
 #[derive(Clone)]
-pub struct Database {
+pub(crate) struct Database {
     pool: PgPool,
 }
 
 impl Database {
     pub async fn new() -> Result<Self> {
-        dotenvy::dotenv().ok();
-        Ok(Self {
-            pool: PgPoolOptions::new()
-                .max_connections(5)
-                .connect(&env::var("DATABASE_URL")?)
-                .await
-                .context("Failed to connect to database")?,
-        })
+        let pool = PgPoolOptions::new()
+            .max_connections(5)
+            .connect(&env::var("DATABASE_URL")?)
+            .await
+            .context("Failed to connect to database")?;
+        
+        log::info!("Connected to database");
+        Ok(Self { pool })
     }
 
     pub async fn store_skin(&self, skin: &Skin) -> Result<PgQueryResult> {
-        Ok(
-            sqlx::query!("SELECT update_skin_price_ema($1, $2)", skin.id, skin.price)
-                .execute(&self.pool)
-                .await?,
-        )
+        let result = sqlx::query!("SELECT update_skin_price_ema($1, $2)", skin.id, skin.price)
+            .execute(&self.pool)
+            .await?;
+        
+        Ok(result)
     }
 }
