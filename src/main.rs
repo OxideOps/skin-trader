4de65aa841ca0c777590ed343fb0a5db06fb6702
Dillon Roller <dillonroller.18@gmail.com>
2024-07-25 22:22:22 -0400

    refactor (#7)

--- src/main.rs
+++ src/main.rs
@@ -9,36 +9,47 @@ use env_logger::{Builder, Env};
 use tokio::signal;
 use tokio_cron_scheduler::{Job, JobScheduler};
 
+fn setup_env() -> Result<()> {
+    // Logger
+    Builder::from_env(Env::default().default_filter_or("info")).init();
+    // Environment variables
+    dotenvy::dotenv()?;
+    Ok(())
+}
+async fn setup_jobs(scheduler: &mut JobScheduler, api: Api, db: Database) -> Result<()> {
+    let job = Job::new_async("0 0 0 * * * *", move |_uuid, _l| {
+        let api = api.clone();
+        let db = db.clone();
+        Box::pin(async move {
+            update_skins(api, db)
+                .await
+                .map_err(|e| log::error!("{e}"))
+                .ok();
+        })
+    })?;
+
+    scheduler.add(job).await?;
+    Ok(())
+}
+
 async fn update_skins(api: Api, db: Database) -> Result<()> {
     for skin in api.get_skins().await? {
         db.store_skin(&skin).await?;
     }
-    Ok(log::info!("Stored skins to database"))
+
+    log::info!("Stored skins to database");
+    Ok(())
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    Builder::from_env(Env::default().default_filter_or("info")).init();
+    setup_env()?;
 
-    let mut scheduler = JobScheduler::new().await?;
     let api = Api::new();
     let db = Database::new().await?;
-    log::info!("Connected to database");
 
-    // Create a job that runs daily at midnight
-    scheduler
-        .add(Job::new_async("0 0 0 * * * *", move |_uuid, _l| {
-            let api = api.clone();
-            let db = db.clone();
-            Box::pin(async move {
-                update_skins(api, db)
-                    .await
-                    .map_err(|e| log::error!("{e}"))
-                    .ok();
-            })
-        })?)
-        .await?;
-
+    let mut scheduler = JobScheduler::new().await?;
+    setup_jobs(&mut scheduler, api, db).await?;
     scheduler.start().await?;
 
     signal::ctrl_c().await?;
