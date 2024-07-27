abec6617e99395a1b047e69031c94f32e185c524
Dillon Roller <dillonroller.18@gmail.com>
2024-07-27 15:28:59 -0400

    fix items table (#25)

--- migrations/20240727191317_create_items_table.sql
+++ migrations/20240727191317_create_items_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE items (
     skin_id BIGINT NOT NULL,
     price BIGINT NOT NULL,
     float_value DECIMAL(10, 8) NOT NULL,
-    created_at DATE,
+    created_at DATE
 );
 
 CREATE INDEX idx_items_skin_id ON items (skin_id);
