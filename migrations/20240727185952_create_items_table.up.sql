589fb8a504c174789f797f3873294e0299a87b59
Dillon-Roller <dillonroller.18@gmail.com>
2024-07-27 15:04:34 -0400

    create `items` table

---
+++ migrations/20240727185952_create_items_table.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE items (
+    id SERIAL PRIMARY KEY,
+    created_at TIMESTAMP WITH TIME ZONE,
+    float_value DECIMAL(10, 8) NOT NULL,
+    price BIGINT NOT NULL,
+    skin_id BIGINT NOT NULL
+);
+
+CREATE INDEX idx_items_skin_id ON items (skin_id);
