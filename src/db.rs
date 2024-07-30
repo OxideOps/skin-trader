1c1030b82e67dacb43e74bd33c06b8a25b5886b6
Daniel Albl <66034631+DanielAlbl@users.noreply.github.com>
2024-07-29 23:40:09 -0600

    Cleanup data fetching (#36)

--- src/db.rs
+++ src/db.rs
@@ -1,5 +1,6 @@
+use crate::api::Sale;
 use anyhow::Result;
-use serde_json::{json, Value};
+use serde_json::{from_value, json, Value};
 use sqlx::{postgres::PgPoolOptions, PgPool};
 use std::env;
 
@@ -37,19 +38,22 @@ impl Database {
         Ok(())
     }
 
-    pub async fn select_json_sales(&self, skin_id: i32) -> Result<Value> {
+    pub async fn select_sales(&self, skin_id: i32) -> Result<Vec<Sale>> {
         let record = sqlx::query!("SELECT json FROM sales WHERE skin_id = $1", skin_id)
             .fetch_one(&self.pool)
             .await?;
 
-        Ok(record.json)
+        Ok(from_value(record.json)?)
     }
 
-    pub async fn select_all_json_sales(&self) -> Result<Vec<(i32, Value)>> {
+    pub async fn select_all_sales(&self) -> Result<Vec<(i32, Vec<Sale>)>> {
         let record = sqlx::query!("SELECT * FROM sales")
             .fetch_all(&self.pool)
             .await?;
 
-        Ok(record.into_iter().map(|r| (r.skin_id, r.json)).collect())
+        record
+            .into_iter()
+            .map(|r| Ok((r.skin_id, from_value(r.json)?)))
+            .collect()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,9 @@ mod scheduler;
 
 use crate::api::{Api, Sale, Wear};
 use crate::db::Database;
-use crate::plotter::plot_data;
+use crate::plotter::plot_by_dates;
 use anyhow::Result;
 use env_logger::{Builder, Env};
-use serde_json::Value;
-use sqlx::types::time::Date;
-use std::collections::HashSet;
 
 fn setup_env() -> Result<()> {
     // Logger
@@ -21,59 +18,26 @@ fn setup_env() -> Result<()> {
     Ok(())
 }
 
-async fn plot_by_floats(db: &Database, skin_id: i32) -> Result<()> {
-    let arr: Vec<Sale> = serde_json::from_value(db.select_json_sales(skin_id).await?)?;
-    let floats: Vec<f64> = arr.iter().map(|sale| sale.float_value.unwrap()).collect();
-    let prices: Vec<f64> = arr.iter().map(|sale| sale.price).collect();
-    plot_data(
-        &floats,
-        &prices,
-        &format!("plots/floats/{skin_id}.png"),
-        &format!("Floats vs Price"),
-        &format!("Float"),
-        &format!("Price"),
-    )?;
-    Ok(())
-}
-
-async fn plot_by_dates(db: &Database, skin_id: i32) -> Result<()> {
-    let arr: Vec<Sale> = serde_json::from_value(db.select_json_sales(skin_id).await?)?;
-    let dates: Vec<Date> = arr.iter().map(|sale| sale.created_at).collect();
-    let prices: Vec<f64> = arr.iter().map(|sale| sale.price).collect();
-    plot_data(
-        &dates,
-        &prices,
-        &format!("plots/dates/{skin_id}.png"),
-        &format!("Dates vs Price"),
-        &format!("Date"),
-        &format!("Price"),
-    )?;
-    Ok(())
+async fn filter_skins(db: &Database, condition: impl Fn(Vec<Sale>) -> bool) -> Result<Vec<i32>> {
+    Ok(db
+        .select_all_sales()
+        .await?
+        .into_iter()
+        .filter_map(|(skin_id, sales)| condition(sales).then_some(skin_id))
+        .collect())
 }
 
 async fn filter_interesting_skins(db: &Database) -> Result<Vec<i32>> {
-    let mut interesting_skins = HashSet::new();
-    for (skin_id, json) in db.select_all_json_sales().await? {
-        if let Value::Array(ref arr) = json {
-            if arr.len() == 500 {
-                let no_weird_values = arr.iter().all(|value| {
-                    let sale: Sale = serde_json::from_value(value.clone()).unwrap();
-                    sale.float_value.is_some() && sale.extras_1.is_null() && sale.phase_id.is_null()
-                });
-                let has_rare_items = arr.iter().any(|value| {
-                    let sale: Sale = serde_json::from_value(value.clone()).unwrap();
-                    sale.float_value
-                        .map(Wear::new)
-                        .filter(|wear| *wear == Wear::FactoryNew)
-                        .is_some()
-                });
-                if no_weird_values && has_rare_items {
-                    interesting_skins.insert(skin_id);
-                }
-            }
-        }
-    }
-    Ok(interesting_skins.into_iter().collect())
+    filter_skins(db, |sales| {
+        sales.len() == 500
+            && sales.iter().all(|sale| {
+                sale.float_value.is_some() && sale.extras_1.is_null() && sale.phase_id.is_null()
+            })
+            && sales
+                .iter()
+                .any(|sale| matches!(sale.float_value.map(Wear::new), Some(Wear::FactoryNew)))
+    })
+    .await
 }
 
 #[tokio::main]
