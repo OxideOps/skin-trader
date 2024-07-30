05c28b4a07b1b9e85e9136c01edf145eb1225114
Dillon-Roller <dillonroller.18@gmail.com>
2024-07-30 15:12:15 -0400

    improve plotter

--- src/plotter.rs
+++ src/plotter.rs
@@ -1,126 +1,138 @@
-// File: src/plotter.rs
-use crate::api;
 use crate::api::Sale;
 use crate::db::Database;
 use anyhow::{bail, Result};
-use api::Date;
 use plotters::prelude::*;
-use std::fmt::Debug;
+use std::ops::Range;
 
-pub enum Plot {
+pub enum PlotType {
     Scatter,
     Line,
     Bar,
 }
 
+pub trait Plottable: Copy + Into<f64> + PartialOrd  {}
+impl<T: Copy + Into<f64> + PartialOrd> Plottable for T {}
+
+pub struct PlotData<X: Plottable, Y: Plottable> {
+    x: Vec<X>,
+    y: Vec<Y>,
+}
+
+impl<X: Plottable, Y: Plottable> PlotData<X, Y> {
+    pub fn new(x: Vec<X>, y: Vec<Y>) -> Result<Self> {
+        if x.len() != y.len() {
+            bail!("Input vectors must have the same length");
+        }
+        Ok(Self { x, y })
+    }
+}
+
 pub async fn plot_by_floats(db: &Database, skin_id: i32) -> Result<()> {
-    let arr: Vec<Sale> = db.select_sales(skin_id).await?;
-    let floats: Vec<f64> = arr.iter().map(|sale| sale.float_value.unwrap()).collect();
-    let prices: Vec<f64> = arr.iter().map(|sale| sale.price).collect();
-    plot_data(
-        &floats,
-        &prices,
+    let sales: Vec<Sale> = db.select_sales(skin_id).await?;
+    let plot_data = PlotData::new(
+        sales.iter().map(|sale| sale.float_value.unwrap()).collect(),
+        sales.iter().map(|sale| sale.price).collect(),
+    )?;
+
+    plot_generic(
+        &plot_data,
         PlotType::Scatter,
         &format!("plots/floats/{skin_id}.png"),
         "Floats vs Price",
         "Float",
         "Price",
-    )?;
-    Ok(())
+    )
 }
 
 pub async fn plot_by_dates(db: &Database, skin_id: i32) -> Result<()> {
-    let arr: Vec<Sale> = db.select_sales(skin_id).await?;
-    let dates: Vec<Date> = arr.iter().map(|sale| sale.created_at).collect();
-    let prices: Vec<f64> = arr.iter().map(|sale| sale.price).collect();
-    plot_data(
-        &dates,
-        &prices,
-        Plot::Scatter,
+    let sales: Vec<Sale> = db.select_sales(skin_id).await?;
+    let plot_data = PlotData::new(
+        sales.iter().map(|sale| sale.created_at).collect(),
+        sales.iter().map(|sale| sale.price).collect(),
+    )?;
+
+    plot_generic(
+        &plot_data,
+        PlotType::Bar,
         &format!("plots/dates/{skin_id}.png"),
         "Dates vs Price",
         "Date",
         "Price",
-    )?;
-    Ok(())
+    )
 }
 
-fn plot_data<T, U>(
-    x: &[T],
-    y: &[U],
-    plot: Plot,
+fn plot_generic<X: Plottable, Y: Plottable>(
+    data: &PlotData<X, Y>,
+    plot_type: PlotType,
     output_file: &str,
     chart_title: &str,
     x_label: &str,
     y_label: &str,
-) -> Result<()>
-where
-    T: Copy + Into<f64> + PartialOrd + Debug,
-    U: Copy + Into<f64> + PartialOrd + Debug,
-{
-    // Ensure input vectors have the same length
-    if x.len() != y.len() {
-        bail!("Input vectors must have the same length")
-    }
-
-    // Convert x and y into a single vector of tuples and find min/max values
-    let data: Vec<(f64, f64)> = x
+) -> Result<()> {
+    let converted_data: Vec<(f64, f64)> = data.x
         .iter()
-        .zip(y.iter())
+        .zip(data.y.iter())
         .map(|(&x, &y)| (x.into(), y.into()))
         .collect();
-    let (min_x, max_x, min_y, max_y) = data.iter().fold(
-        (
-            f64::INFINITY,
-            f64::NEG_INFINITY,
-            f64::INFINITY,
-            f64::NEG_INFINITY,
-        ),
-        |(min_x, max_x, min_y, max_y), &(x, y)| {
-            (min_x.min(x), max_x.max(x), min_y.min(y), max_y.max(y))
-        },
-    );
 
-    // Set up the plot area
+    let (x_range, y_range) = find_bounds(&converted_data);
+
     let root = BitMapBackend::new(output_file, (1600, 1200)).into_drawing_area();
     root.fill(&WHITE)?;
 
-    // Define the chart area
     let mut chart = ChartBuilder::on(&root)
         .caption(chart_title, ("sans-serif", 50))
         .margin(50)
         .x_label_area_size(50)
         .y_label_area_size(50)
-        .build_cartesian_2d(min_x..max_x, min_y..max_y)?;
+        .build_cartesian_2d(x_range.clone(), y_range.clone())?;
 
-    // Configure and draw the chart
     chart
         .configure_mesh()
         .x_desc(x_label)
         .y_desc(y_label)
         .draw()?;
 
-    match plot {
-        Plot::Scatter => {
+    match plot_type {
+        PlotType::Scatter => {
             chart.draw_series(
-                data.iter()
+                converted_data.iter()
                     .map(|point| Circle::new(*point, 3, RED.mix(0.5))),
             )?;
         }
-        Plot::Line => {
-            todo!()
+        PlotType::Line => {
+            chart.draw_series(LineSeries::new(
+                converted_data,
+                &RED,
+            ))?;
         }
-        Plot::Bar => {
-            todo!()
+        PlotType::Bar => {
+            let bar_width = (x_range.end - x_range.start) / (converted_data.len() as f64) * 0.8;
+            chart.draw_series(
+                converted_data.iter().map(|&(x, y)| {
+                    Rectangle::new(
+                        [(x - bar_width / 2.0, y_range.start), (x + bar_width / 2.0, y)],
+                        RED.filled(),
+                    )
+                }),
+            )?;
         }
     }
 
-    // Add a title to the plot
     let text_style = ("sans-serif", 30).into_font();
     root.draw_text(chart_title, &text_style.into(), (300, 30))?;
 
-    // Save the plot
     root.present()?;
 
     Ok(())
+}
+
+fn find_bounds(data: &[(f64, f64)]) -> (Range<f64>, Range<f64>) {
+    let (min_x, max_x, min_y, max_y) = data.iter().fold(
+        (f64::INFINITY, f64::NEG_INFINITY, f64::INFINITY, f64::NEG_INFINITY),
+        |(min_x, max_x, min_y, max_y), &(x, y)| {
+            (min_x.min(x), max_x.max(x), min_y.min(y), max_y.max(y))
+        },
+    );
+    (min_x..max_x, min_y..max_y)
 }
