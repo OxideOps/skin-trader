58cd5cf9fae0ace53ca645daed96817ece5253ca
Dillon-Roller <dillonroller.18@gmail.com>
2024-08-10 01:58:28 -0400

    refactor project into `workspace`

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -1,3 +1,10 @@
 pub mod db;
 pub mod http;
 pub mod ws;
+
+use env_logger::{Builder, Env};
+
+pub fn setup_env() {
+    Builder::from_env(Env::default().default_filter_or("info")).init();
+    dotenvy::dotenv().ok();
+}
--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -3,6 +3,7 @@ mod util;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    api::setup_env();
     let _db = api::db::Database::new().await?;
     Ok(())
 }
