0438e01926e57031d8c07183e4ee7d13289b65a2
Dillon-Roller <dillonroller.18@gmail.com>
2024-08-10 01:57:30 -0400

    refactor project into `workspace`

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -2,6 +2,7 @@ mod scheduler;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    api::setup_env();
     let ws = api::ws::WsClient::connect().await?;
     ws.start().await?;
     Ok(())
