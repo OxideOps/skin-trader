3855f000437b363464ae9165f4bcc55e136c6609
Dillon-Roller <dillonroller.18@gmail.com>
2024-08-12 22:56:09 -0400

    update price

--- api/src/http.rs
+++ api/src/http.rs
@@ -127,6 +127,18 @@ impl HttpClient {
         }
     }
 
+    pub async fn update_price(&self, id: &str, price: i32) -> Result<()> {
+        let url = format!("{BASE_URL}/market/update_price/single");
+
+        let payload = json!({
+            "app_id": CS2_APP_ID,
+            "id": id,
+            "price": price,
+        });
+
+        self.post(url, payload).await
+    }
+
     pub async fn sell_item(&self, id: &str, price: i32) -> Result<()> {
         let url = format!("{BASE_URL}/market/relist/single");
 
