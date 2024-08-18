9aa4dd6bcfcbeaa5015c5b34d5ccf79ee0cb79c7
Dillon-Roller <dillonroller.18@gmail.com>
2024-08-18 12:01:20 -0400

    sync bitskins data

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,10 +1,14 @@
+use api::CS2_APP_ID;
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     api::setup_env();
     let db = api::Database::new().await?;
     let client = api::HttpClient::new();
 
-    db.sync_bitskins_data(&client).await?;
+    //db.sync_bitskins_data(&client).await?;
+    
+    client.fetch_skins(CS2_APP_ID).await?;
 
     Ok(())
 }
