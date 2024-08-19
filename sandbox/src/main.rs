7286cbf48bd6e4ebe224b44843eb67247eb821e1
Dillon Roller <dillonroller.18@gmail.com>
2024-08-19 18:30:28 -0400

    `api` is now `bitskins` (#106)

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,10 +1,10 @@
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    api::setup_env();
-    let db = api::Database::new().await?;
-    let client = api::HttpClient::new();
+    bitskins::setup_env();
+    let db = bitskins::Database::new().await?;
+    let client = bitskins::HttpClient::new();
 
-    api::sync_bitskins_data(&db, &client).await?;
+    bitskins::sync_bitskins_data(&db, &client).await?;
 
     Ok(())
 }
