5d9c79a3300fd5c751fd6491e74dd74af31a6ebd
Dillon-Roller <dillonroller.18@gmail.com>
2024-08-20 09:19:55 -0400

    reduce rate to 4 requests per second to play it safe

--- bitskins/src/update.rs
+++ bitskins/src/update.rs
@@ -159,7 +159,7 @@ async fn process_skin(
 pub async fn sync_bitskins_data(db: &Database, client: &HttpClient) -> Result<()> {
     let skins = client.fetch_skins().await?;
 
-    let rate_limiter = Arc::new(RateLimiter::new(http::GLOBAL_RATE));
+    let rate_limiter = Arc::new(RateLimiter::new(http::GLOBAL_RATE - 1));
     let client = Arc::new(client.clone());
 
     let tasks: Vec<_> = skins
