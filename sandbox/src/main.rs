528a95a06f29770fac2ffbcb37b1cc94eb2791e6
Dillon-Roller <dillonroller.18@gmail.com>
2024-08-22 18:52:07 -0400

    improve code

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,7 +1,7 @@
-use bitskins::{sync_bitskins_data, Database, HttpClient, Result};
+use bitskins::{sync_bitskins_data, Database, HttpClient};
 
 #[tokio::main]
-async fn main() -> Result<()> {
+async fn main() -> anyhow::Result<()> {
     bitskins::setup_env();
     let db = Database::new().await?;
     let client = HttpClient::new();
