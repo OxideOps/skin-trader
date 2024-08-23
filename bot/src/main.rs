9eb5395db86fc55f3404f7fbaaa0d0ecbaa4f686
Dillon-Roller <dillonroller.18@gmail.com>
2024-08-23 17:49:32 -0400

    get as much `MarketData` as possible

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -7,10 +7,9 @@ use trader::Trader;
 #[tokio::main]
 async fn main() -> Result<()> {
     bitskins::setup_env();
-    // let trader = Trader::new().await?;
-    // let ws = WsClient::connect(|channel, ws_data| trader.process_data(channel, ws_data)).await?;
-    //
-    // ws.start().await?;
-    bitskins::HttpClient::new().fetch_all_market_data(720).await?;
+    let trader = Trader::new().await?;
+    let ws = WsClient::connect(|channel, ws_data| trader.process_data(channel, ws_data)).await?;
+    
+    ws.start().await?;
     Ok(())
 }
