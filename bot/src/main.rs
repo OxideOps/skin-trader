f202cd5e9a99e1bf8241558b5686e0ce1b5e6eb9
Dillon-Roller <dillonroller.18@gmail.com>
2024-08-24 13:31:59 -0400

    MarketData

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -7,12 +7,9 @@ use trader::Trader;
 #[tokio::main]
 async fn main() -> Result<()> {
     bitskins::setup_env();
-    bitskins::HttpClient::new()
-        .fetch_market_items_for_skin(720)
-        .await?;
-    // let trader = Trader::new().await?;
-    // let ws = WsClient::connect(|channel, ws_data| trader.process_data(channel, ws_data)).await?;
-    //
-    // ws.start().await?;
+    let trader = Trader::new().await?;
+    let ws = WsClient::connect(|channel, ws_data| trader.process_data(channel, ws_data)).await?;
+    
+    ws.start().await?;
     Ok(())
 }
