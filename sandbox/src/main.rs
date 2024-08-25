4e090b46f3866129c0bfdab42514934b92b32247
Dillon Roller <dillonroller.18@gmail.com>
2024-08-25 02:15:35 -0400

    sync concurrently (#135)

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,5 +1,7 @@
 use bitskins::{sync_market_data, sync_sales_data, Database, HttpClient};
 
+use tokio::try_join;
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     bitskins::setup_env();
@@ -7,8 +9,11 @@ async fn main() -> anyhow::Result<()> {
     let client = HttpClient::new();
 
     db.flush_all().await?;
-    sync_market_data(&db, &client).await?;
-    sync_sales_data(&db, &client).await?;
+
+    try_join!(
+        sync_market_data(&db, &client),
+        sync_sales_data(&db, &client)
+    )?;
 
     Ok(())
 }
