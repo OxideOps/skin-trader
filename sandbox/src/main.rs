fa139bc5fbcdac977fb1e9148a45c80c5e29bed7
Daniel Albl <66034631+DanielAlbl@users.noreply.github.com>
2024-08-30 19:46:17 -0600

    Update listings for inventory (#166)

--- sandbox/src/main.rs
+++ sandbox/src/main.rs
@@ -1,4 +1,4 @@
-use bitskins::{sync_data, Database, HttpClient};
+use bitskins::{Database, HttpClient};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -6,7 +6,12 @@ async fn main() -> anyhow::Result<()> {
     let db = Database::new().await?;
     let client = HttpClient::new();
 
-    sync_data(&db, &client).await?;
+    for item in client.fetch_inventory().await? {
+        let stats = db.get_price_statistics(item.skin_id).await?;
+        if let Some(mean) = stats.mean_price {
+            client.list_item(&item.id, mean).await?;
+        }
+    }
 
     Ok(())
 }
