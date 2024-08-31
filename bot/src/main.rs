c2d57b6bee7ae82ed775e18bd8eb8875825fbb6e
Dillon-Roller <dillonroller.18@gmail.com>
2024-08-30 21:06:33 -0400

    update

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -7,10 +7,9 @@ use trader::Trader;
 #[tokio::main]
 async fn main() -> Result<()> {
     bitskins::setup_env();
-    dbg!(bitskins::HttpClient::new().delist_item("4790544").await?);
-    // let trader = Trader::new().await?;
-    // let ws = WsClient::connect(|channel, ws_data| trader.process_data(channel, ws_data)).await?;
-    //
-    // ws.start().await?;
+    let trader = Trader::new().await?;
+    let ws = WsClient::connect(|channel, ws_data| trader.process_data(channel, ws_data)).await?;
+
+    ws.start().await?;
     Ok(())
 }
