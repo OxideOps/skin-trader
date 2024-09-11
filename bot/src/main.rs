385737d3a86be2d5155fb7322699717218ec2cca
Dillon-Roller <dillonroller.18@gmail.com>
2024-09-10 22:05:45 -0400

    refactor env setup

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -3,7 +3,7 @@ mod trader;
 use anyhow::Result;
 use bitskins::Updater;
 use bitskins::WsClient;
-use env_logger::{Builder, Env};
+use env_logger::Builder;
 use log::LevelFilter;
 use tokio::try_join;
 use trader::Trader;
@@ -11,15 +11,11 @@ use trader::Trader;
 #[tokio::main]
 async fn main() -> Result<()> {
     setup_env();
-    // bitskins::setup_env();
-    // let trader = Trader::new().await?;
-    // let updater = Updater::new().await?;
-    // let ws = WsClient::connect(|channel, ws_data| trader.process_data(channel, ws_data)).await?;
-    //
-    // try_join!(updater.sync_new_sales(), ws.start())?;
-    let c = dmarket::Client::new()?;
-    dbg!(c.get_market_items("a8db").await?);
-    log::info!("here");
+    let trader = Trader::new().await?;
+    let updater = Updater::new().await?;
+    let ws = WsClient::connect(|channel, ws_data| trader.process_data(channel, ws_data)).await?;
+
+    try_join!(updater.sync_new_sales(), ws.start())?;
     Ok(())
 }
 
