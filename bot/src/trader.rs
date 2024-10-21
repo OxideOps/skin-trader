a790bb7df61821eae13526378ae08123d454046f
Daniel Albl <66034631+DanielAlbl@users.noreply.github.com>
2024-10-21 10:48:40 -0600

    Update (#213)

--- bot/src/trader.rs
+++ bot/src/trader.rs
@@ -7,7 +7,7 @@ use std::cmp::Ordering;
 const MAX_PRICE_BALANCE_THRESHOLD: f64 = 0.20;
 const SALES_FEE: f64 = 0.1;
 const MIN_PROFIT_MARGIN: f64 = 0.1;
-const MIN_SALE_COUNT: i32 = 400;
+const MIN_SALE_COUNT: i32 = 500;
 const MIN_SLOPE: f64 = 0.0;
 
 #[derive(Clone)]
