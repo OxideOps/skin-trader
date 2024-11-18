503f5396b98e34065d8ff9f1db4a7137b1414434
Dillon-Roller <dillonroller.18@gmail.com>
2024-11-18 13:16:19 -0500

    db schema

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -12,8 +12,7 @@ use trader::Trader;
 #[tokio::main]
 async fn main() -> Result<()> {
     setup_env();
-    // start_bitskins().await?;
-    start_dmarket().await?;
+    try_join!(start_bitskins(), start_dmarket())?;
     Ok(())
 }
 
