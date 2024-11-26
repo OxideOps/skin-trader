c2145c8be1e41b05c93d7b7a3b9044bf100d2242
daniel.albl <daniel.albl@vivint.com>
2024-11-25 21:39:47 -0700

    Commit systemd stuff for running and updating the bot

---
+++ scripts/check-skin-trader.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd /home/oxideops/skin-trader
+
+git fetch
+
+LOCAL_HASH=$(git rev-parse @)
+REMOTE_HASH=$(git rev-parse @{u})
+
+if [ "$LOCAL_HASH" != "$REMOTE_HASH" ]; then
+    git reset --hard "$REMOTE_HASH"
+    sqlx migrate run
+    cargo build -p bot -r
+    sudo systemctl restart skin-trader
+fi
+
